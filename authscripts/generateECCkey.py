55624a02459c1974884e78cca9b630c1f1836008
Geoff Twardokus <gdt5762@rit.edu>
2020-04-18 11:16:13 -0400

    Some testing modifications, nothing substantive

--- authscripts/generateECCkey.py
+++ authscripts/generateECCkey.py
@@ -1,7 +1,7 @@
 import ecdsa
 
-public_keypath = "/home/user/ecc_public.key"
-private_keypath="/home/user/ecc_private.key"
+public_keypath = "/home/administrator/v2v-capstone/keys/ecc_public.key"
+private_keypath="/home/administrator/v2v-capstone/keys/ecc_private.key"
 sk = ecdsa.SigningKey.generate(curve=ecdsa.NIST256p)
 with open(private_keypath, 'w') as output:
     output.write(sk.to_string())
