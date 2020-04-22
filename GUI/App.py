d6c18c8784b60b3d1174926c09622adf3357aae7
jponicki <jxp3224@rit.edu>
2020-04-22 17:00:12 -0400

    added Car class and functionality for updating location and pic

--- GUI/App.py
+++ GUI/App.py
@@ -2,6 +2,49 @@ from tkinter.ttk import Frame
 import tkinter as tk
 from PIL import Image
 from PIL import ImageTk
+import time
+
+
+class Car:
+    def __init__(self, CarID, name, x, y, pic):
+        self.CarID = CarID
+        self.name = name
+        self.x = x
+        self.y = y
+        self.pic = pic
+
+# used to determine if a new car has been added
+# key: Car ID, value: Car object
+carDict = {}
+
+
+# helper function for whatPos
+def setPicCoord(c, pic, x, y):
+    c.pic = pic
+    c.x = x
+    c.y = y
+
+
+# updates the coordinates and picture of the Car object
+# based on the direction it has moved.
+def whatPos(c, x, y):
+    if c.x < x and c.y < y:
+        setPicCoord(c, c.name + "NE.png", x, y)
+    elif c.x > x and c.y < y:
+        setPicCoord(c, c.name + "NW.png", x, y)
+    elif c.x < x and c.y > y:
+        setPicCoord(c, c.name + "SE.png", x, y)
+    elif c.x > x and c.y > y:
+        setPicCoord(c, c.name + "SW.png", x, y)
+    elif c.x < x and c.y == y:
+        setPicCoord(c, c.name + "E.png", x, y)
+    elif c.x > x and c.y == y:
+        setPicCoord(c, c.name + "W.png", x, y)
+    elif c.x == x and c.y < y:
+        setPicCoord(c, c.name + "N.png", x, y)
+    else:
+        setPicCoord(c, c.name + "S.png", x, y)
+
 
 root = tk.Tk()
 root.title("Secure V2V Communication Simulator")
@@ -37,13 +80,16 @@ Car2 = ImageTk.PhotoImage(Image.open("pic/Car3NE.png"))
 canvas.create_image(700, 450, image=Car2, anchor=tk.CENTER)
 
 #adds output panel on right
-textWidget = tk.Text(root, height=800, width=500)
+textWidget = tk.Text(root, height=800, width=500, font=36)
 textWidget.pack(side=tk.RIGHT)
 
 textWidget.tag_configure("green", foreground="green")
-textWidget.insert(tk.END, "Testing 123\n", "green")
-textWidget.tag_configure("orange", foreground="orange")
-textWidget.insert(tk.END, "Testing 123\n", "orange")
+textWidget.insert(tk.END, "Car:1 message has authenticated\n", "green")
+textWidget.insert(tk.END, "Car:1 is located at (50,50)\n", "green")
+
+textWidget.tag_configure("orange", foreground="purple")
+textWidget.insert(tk.END, "Car:2 message has authenticated\n", "orange")
+textWidget.insert(tk.END, "Car:2 is located at (700,450)\n", "orange")
 
 
 root.mainloop()
