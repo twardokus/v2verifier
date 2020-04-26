abff7bfb43a09bfcefe332958cceca860f4b8ad7
Geoff Twardokus <gdt5762@rit.edu>
2020-04-25 22:33:16 -0400

    Added UDP sink to the WiFi receiver GRC and a live Scapy listener to parse packets as they come in from the USRP. Tested and working :)

--- receiver.py
+++ receiver.py
@@ -5,20 +5,25 @@ from fastecdsa import keys, curve
 from scapy.all import *
 import binascii
 
+from time import sleep
+
 pcapFileLocation = "/home/administrator/Desktop/out.pcap"
 
 # load the public key - debugging
 publicKey = keys.import_key("/home/administrator/v2v-capstone/keys/other_p256.pub",curve=curve.P256, public=True)
 
-
-# get the packets
-packets = parsePcapFile(pcapFileLocation)
-
-for packet in packets:
-    
-    payload = str(binascii.hexlify(packet[Raw].load))
+def processPacket(payload):
+    #payload = str(binascii.hexlify(packet[Raw].load))
     data = extractData(payload)
-
     status = verifyMessage(data[1],data[2],data[0],publicKey)
 
+    print "Message " + data[0].decode('hex').replace("\n","") + "\" is " + "valid" if status else "invalid"
 
+sniff(iface="lo", filter="udp and port 4444", prn=lambda x: processPacket(str(binascii.hexlify(x.load))[130:]))
+
+
+"""
+print "Listening for traffic on port 4444"
+
+sniff(iface="lo", filter="udp and port 4444", prn=lambda x: processPacket(x))
+"""
