6db714d3bc8d6ee8867a8fc6b5be8af364d728d4
jponicki <jxp3224@rit.edu>
2020-04-27 01:47:26 -0400

    displays og coords and cars now turn

--- GUI/App.py
+++ GUI/App.py
@@ -57,35 +57,34 @@ def setPicCoord(c, pic, x, y):
     carDict[c].pic = pic
     carDict[c].x = x
     carDict[c].y = y
-    carDict[c].i = ImageTk.PhotoImage(Image.open(c.name))
+    carDict[c].i = ImageTk.PhotoImage(Image.open(carDict[c].pic))
 
 
 # updates the coordinates and picture of the Car object
 # based on the direction it has moved.
 def whatPos(c, x, y):
     print("Entered whatPos")
-    if c.x < x and c.y < y:
+    if carDict[c].x < x and carDict[c].y < y:
         setPicCoord(c, "pic/" + carDict[c].name + "NE.png", x, y)
-    elif c.x > x and c.y < y:
+    elif carDict[c].x > x and carDict[c].y < y:
         setPicCoord(c, "pic/" + carDict[c].name + "NW.png", x, y)
-    elif c.x < x and c.y > y:
+    elif carDict[c].x < x and carDict[c].y > y:
         setPicCoord(c, "pic/" + carDict[c].name + "SE.png", x, y)
-    elif c.x > x and c.y > y:
+    elif carDict[c].x > x and carDict[c].y > y:
         setPicCoord(c, "pic/" + carDict[c].name + "SW.png", x, y)
-    elif c.x < x and c.y == y:
+    elif carDict[c].x < x and carDict[c].y == y:
         setPicCoord(c, "pic/" + carDict[c].name + "E.png", x, y)
-    elif c.x > x and c.y == y:
+    elif carDict[c].x > x and carDict[c].y == y:
         setPicCoord(c, "pic/" + carDict[c].name + "W.png", x, y)
-    elif c.x == x and c.y < y:
+    elif carDict[c].x == x and carDict[c].y < y:
         setPicCoord(c, "pic/" + carDict[c].name + "N.png", x, y)
     else:
-        setPicCoord(c, "pic/" + carDict[c].name + "S.png", x, y)
+        setPicCoord(c, carDict[c].name + "S.png", x, y)
 
 
 root = tk.Tk()
 root.title("Secure V2V Communication Simulator")
-#root.state("zoomed")  # makes full screen
-print("Line 75")
+root.state("zoomed")  # makes full screen
 topFrame = Frame(root, width=1400, height=800)  # Added "container" Frame.
 topFrame.pack(side=tk.LEFT)
 # create the drawing canvas
@@ -119,38 +118,31 @@ textWidget.pack(side=tk.RIGHT)
 # sends to whatPos function to update x, y and pic
 # modelled after trace file in mycourses
 def newPacket(carid, message, x, y):
-    
-    print("x and y into newPacket")
-    print(x)
-    print(y)
-    print("==============")
-
-    x = int(x)
-    y = int(y)
+    newx = int(x)
+    newy = int(y)
 
 #    print(str(x))
 #    print(str(y))
 
-    x = (x - 8500)/2
-    y = (y - 8000)/2
-
-    print(str(x))
-    print(str(y))
+    newx = (newx - 8500)/2
+    newy = (newy - 8000)/2
 
     print("Entered newPacket")
     if carid in carDict:
         canvas.delete(carDict[carid].i)
-        whatPos(carid, x, y)
+        whatPos(carid, newx, newy)
         canvas.create_image(carDict[carid].x, carDict[carid].y, image=carDict[carid].i, anchor=tk.CENTER)
+        print("image: " + carDict[carid].name)
         textWidget.insert(tk.END, "Car:" + str(carid) + " is at location (" + str(x) + "," + str(y) + ")\n")
     else:
         length = len(carDict) + 1
         name = "Car" + str(length)
         pic = name + "N.png"
-        c = Car(carid, "pic/" + name + "N.png", x, y, pic, ImageTk.PhotoImage(Image.open("pic/" + name + "N.png")))
+        c = Car(carid, name, newx, newy, pic, ImageTk.PhotoImage(Image.open("pic/" + name + "N.png")))
         carDict[carid] = c
         canvas.create_image(carDict[carid].x, carDict[carid].y, image=carDict[carid].i, anchor=tk.CENTER)
         textWidget.insert(tk.END, "Car:" + str(carid) + " is at location (" + str(x) + "," + str(y) + ")\n")
+
 
 s = socket()
 port = 6666
@@ -160,11 +152,7 @@ c, addr = s.accept()
 
 
 """
-newPacket(1, "hello", 50, 40)
-newPacket(2, "hello2", 90, 100)
 
-newPacket(1, "bye", 400, 500)
-newPacket(2, "bye2", 350, 200)
 
 HOST = '127.0.0.1'
 PORT = 6666
@@ -186,12 +174,4 @@ receive_thread.start()
 
 root.mainloop()
 
-# UDP_IP = "127.0.0.1"
-# UDP_PORT = 5005
-# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# sock.bind((UDP_IP, UDP_PORT))
-# while True:
-#     data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-#     print("received message:", data)
-#
 
