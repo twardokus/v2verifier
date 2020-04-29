e56a6bc4cb74f367baf998cb152622e841a09564
Geoff Twardokus <gdt5762@rit.edu>
2020-04-29 10:30:51 -0400

    Two vehicles now rendered and moving properly on the screen

--- receiver.py
+++ receiver.py
@@ -8,14 +8,14 @@ import binascii
 from time import sleep
 
 from socket import AF_INET, SOCK_STREAM
-
+import math
 
 pcapFileLocation = "/home/administrator/Desktop/out.pcap"
 
 # load the public key
 publicKey = keys.import_key("/home/administrator/v2v-capstone/keys/other_p256.pub",curve=curve.P256, public=True)
 
-def processPacket(payload,s):
+def processPacket(payload,s,lock):
     data = extractData(payload)
     status = verifyMessage(data[1],data[2],data[0],publicKey)
     statusText = "valid" if status else "invalid"
@@ -23,22 +23,36 @@ def processPacket(payload,s):
     result = "Message is " + statusText + ", contents: " + data[0].decode('hex').replace("\n","")
     
     vehicleData = data[0].decode('hex').replace("\n","")
-    vehicleData += "," + str(result)
+    vehicleData += "," + str(status)
 
-    s.send(vehicleData.encode())
+    with lock:
+        s.send(vehicleData.encode())
 
-def startOwnVehicle(vehicleNo, socket):
+def runSelf(vehicleNo, socket, lock):
 
-    print ""
+    trace = open("v" + str(vehicleNo) + "path")
+    for i in range(0,400):
+        vehicleData = str(vehicleNo) + "," + trace.readline()
+        vehicleData += ",True"
 
-def runSelf():
+        #data = vehicleData.split(",")
 
-    print ""
+        sleep(0.5)
 
-def listen(s):
+        #data[1] = str(abs(int(data[1])))
+        #data[2] = str(int(data[2]) - 3580)
+
+        #vehicleData = ",".join(data)
+
+        with lock:
+            print "Sending own data "
+            print vehicleData
+            socket.send(vehicleData.encode())
+
+def listen(s,lock):
 
 #    s = socket.socket()
 #    s.connect(('127.0.0.1',6666))
-
-    sniff(iface="lo", filter="udp and port 4444", prn=lambda x: processPacket(str(binascii.hexlify(x.load))[130:],s))
+    print "Listening"
+    sniff(iface="lo", filter="udp and port 4444", prn=lambda x: processPacket(str(binascii.hexlify(x.load))[130:],s,lock))
 
--- rxMain.py
+++ rxMain.py
@@ -1,7 +1,9 @@
 from receiver import *
+from transmitter import loadTracesFromFile, writeVehicleTraceFiles
 
 import os
 import socket
+import threading
 
 def checkIfRoot():
     if os.geteuid() != 0:
@@ -12,7 +14,19 @@ def checkIfRoot():
 if __name__ == "__main__":
     checkIfRoot()
     
+    traces = loadTracesFromFile(os.getcwd() + "/traces.csv")
+    writeVehicleTraceFiles(traces)
+
+
     s = socket.socket()
     s.connect(('127.0.0.1',6666))
+
+    ownVehicleID = 1
+
+    lock = threading.Lock()
+
+    run = threading.Thread(target=runSelf, args=(ownVehicleID, s, lock,))
+    run.start()
     
-    listen(s)
+    listen = threading.Thread(target=listen, args=(s,lock,))
+    listen.start()
