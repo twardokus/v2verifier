0db92667deb83912337e676b4958dbaee694ea1e
Geoff Twardokus <gdt5762@rit.edu>
2020-04-28 22:10:47 -0400

    wrapping up for tonight

--- rxMain.py
+++ rxMain.py
@@ -1,4 +1,13 @@
 from receiver import runReceiver
 
+import os
+
+def checkIfRoot():
+    if os.geteuid() != 0:
+        print "Error - you must be root! Try running with sudo"
+        exit(1)
+
+
 if __name__ == "__main__":
+#    checkIfRoot()
     runReceiver()
