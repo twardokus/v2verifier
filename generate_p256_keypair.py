4f84202e81843c9f14a2357c1dfc7049e74b080d
Geoff Twardokus <gdt5762@rit.edu>
2020-05-03 13:44:50 -0400

    Deleted dead code and added comments

--- generate_p256_keypair.py
+++ generate_p256_keypair.py
@@ -2,31 +2,16 @@ from fastecdsa import keys, curve, ecdsa
 from fastecdsa.keys import import_key, export_key
 from hashlib import sha256
 
+"""
+Little in this file is customized - the keys are generated and stored
+using the standard functions from the imported cryptographic libraries.
+"""
+
+# The directory tree needs to be created before running this script
 publicKeyPath = "/home/administrator/v2v-capstone/keys/p256.pub"
 privateKeyPath = "/home/administrator/v2v-capstone/keys/p256.key"
 
 privateKey, publicKey = keys.gen_keypair(curve.P256)
 
-print privateKey
-print publicKey
-
 export_key(privateKey, curve=curve.P256, filepath=privateKeyPath)
 export_key(publicKey, curve=curve.P256, filepath=publicKeyPath)
-
-"""
-# Test code
-
-def testKeys():
-	private, public = import_key(privateKeyPath)
-
-	print private
-	print public
-
-	message = "Test message"
-
-	r, s = ecdsa.sign(message, private, hashfunc=sha256)
-	print ecdsa.verify((r,s),message, public, hashfunc=sha256)
-
-if __name__=="__main__":
-	testKeys()
-"""
