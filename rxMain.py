5b843b3ceda5370c3e940225187a2910199ae43e
Geoff Twardokus <gdt5762@rit.edu>
2020-05-03 13:22:02 -0400

    Updated comments in this file

--- rxMain.py
+++ rxMain.py
@@ -12,21 +12,29 @@ def checkIfRoot():
 
 
 if __name__ == "__main__":
+    
+    # This script needs root access in order to connect to a TCP socket
     checkIfRoot()
     
     traces = loadTracesFromFile(os.getcwd() + "/traces.csv")
     writeVehicleTraceFiles(traces)
 
-
+    # TCP connection to the GUI application's listener
     s = socket.socket()
     s.connect(('127.0.0.1',6666))
 
+    # Change this at will - can be any positive integer as long as that vehicle
+    # has a trace in the traces.csv file
     ownVehicleID = 2
 
     lock = threading.Lock()
-
+    
+    # This is the thread that updates the "vehicle" that is represented by
+    # by the receiving USRP. No actual transmission for this vehicle.
     run = threading.Thread(target=runSelf, args=(ownVehicleID, s, lock,))
     run.start()
     
+    # This is the thread that accepts data from the other vehicle (via USRP)
+    # and sends it to the GUI
     listen = threading.Thread(target=listen, args=(s,lock,))
     listen.start()
