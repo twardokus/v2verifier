7762b096ffdcf32fd4ac866e4f08ada2381cea11
Geoff Twardokus <gdt5762@rit.edu>
2020-05-03 13:36:28 -0400

    Added comments for documentation and deleted debugging print statements + old code that was commented out.

--- receiver.py
+++ receiver.py
@@ -13,6 +13,17 @@ import math
 # load the public key of the other "vehicle" (the host connected to the other USRP)
 publicKey = keys.import_key("/home/administrator/v2v-capstone/keys/other_p256.pub",curve=curve.P256, public=True)
 
+"""
+Function to extract the data string (format id,x-coord,y-coord) from the 
+payload of the received DSRC message
+
+Inputs:
+
+payload -   the hexadecimal string representing everything above Layer 2,
+            i.e. the WSMP message and embedded ieee1609Dot2Data structure
+s       -   a TCP socket connected to the GUI application
+lock    -   a thread lock passed from rxMain.py
+"""
 
 def processPacket(payload,s,lock):
     
@@ -27,31 +38,35 @@ def processPacket(payload,s,lock):
     with lock:
         s.send(vehicleData.encode())
 
+"""
+Function to send data about the receiver's "vehicle" to the GUI application
+
+Inputs:
+
+vehicleNo   - the id number, used to open the correct trace file
+socket      - the TCP socket connected to the GUI application
+lock        - a thread lock passed from rxMain.py
+"""
 def runSelf(vehicleNo, socket, lock):
 
     trace = open("v" + str(vehicleNo) + "path")
+
     for i in range(0,400):
         vehicleData = str(vehicleNo) + "," + trace.readline()
         vehicleData += ",True"
 
-        #data = vehicleData.split(",")
-
+        # sleeping for 0.5 seconds keeps the display update rate reasonable
         sleep(0.5)
 
-        #data[1] = str(abs(int(data[1])))
-        #data[2] = str(int(data[2]) - 3580)
-
-        #vehicleData = ",".join(data)
-
         with lock:
-#            print "Sending own data "
-#            print vehicleData
             socket.send(vehicleData.replace("\n","").encode())
 
 def listen(s,lock):
 
-#    s = socket.socket()
-#    s.connect(('127.0.0.1',6666))
+    # print a console message to confirm the network connection is active
     print "Listening"
+
+    # listen on localhost:4444 which is the UDP sink from GNURadio in wifi_rx.py
+    # received packets are passed to processPacket() for data extraction
     sniff(iface="lo", filter="udp and port 4444", prn=lambda x: processPacket(str(binascii.hexlify(x.load))[130:],s,lock))
 
