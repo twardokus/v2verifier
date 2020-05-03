36874a33ca66f6460f603829846a3d934999e982
Geoff Twardokus <gdt5762@rit.edu>
2020-05-03 14:06:34 -0400

    Added documentation

--- txMain.py
+++ txMain.py
@@ -1,9 +1,13 @@
+"""
+This script handles the USRP playing the "transmitter" role. The host that runs
+wifi_tx.py should run this script.
+"""
+
 from transmitter import *
 
 import os
 
 if __name__ == "__main__":
-
 
     traces = loadTracesFromFile(os.getcwd() + "/traces.csv")
     writeVehicleTraceFiles(traces)
