9cfbde31235f7638157216b2a8bb6bb48783168c
Geoff Twardokus <gdt5762@rit.edu>
2020-05-24 14:53:33 -0400

    Should have been part of previous commit

--- receiver.py
+++ receiver.py
@@ -57,6 +57,11 @@ def processPacket(payload,s,lock):
 	isRecent = verifyTime(elapsedMicroseconds)
 	
 	# verify the signature
+	print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+	print [data[0]]
+	print [data[3]]
+	print [data[0] + data[3].encode('hex')]
+	print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 	isValidSig = verifySignature(data[1],data[2],(data[0] + data[3]),publicKey)
 
 	# decode and break up the BSM data string "x,y"
