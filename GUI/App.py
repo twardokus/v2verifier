d3b5414793cbb8b58ae2432ff47f97f27ec0d0cb
Geoff Twardokus <gdt5762@rit.edu>
2020-05-23 20:48:53 -0400

    Updated GUI layout and got a thread lock set on the output box to stop messages from overlapping

--- GUI/App.py
+++ GUI/App.py
@@ -11,9 +11,22 @@ import json
 
 
 class GUI:
+	
+
 	def __init__(self, root):
+
+		self.threadlock = threading.Lock()
+		self.receivedPacketCount = 0
+		self.processedPacketCount = 0
+		self.authenticatedPacketCount = 0
+		self.intactPacketCount = 0
+		self.ontimePacketCount = 0
+
 		self.root = root
-		root.title("V2X Communications - Security Testbed")
+		root.title("V2X Communications - Security Testbed")	
+
+		self.root.grid_rowconfigure(1, weight=1)
+		self.root.grid_columnconfigure(1, weight=1)
 
 		CANVAS_HEIGHT = 600
 		CANVAS_WIDTH = 800
@@ -29,7 +42,8 @@ class GUI:
 		self.textWidget.tag_configure("information", foreground="orange")
 
 		# create another textbox to display counters
-		self.counters = tk.Text(root,font=36,bg="white",borderwidth=2).grid(row=0,column=1,sticky="n")
+		self.counters = tk.Text(root,font=36,bg="white",borderwidth=2)
+
 		# draw horizontal lines on the canvas
 		x1 = 0
 		x2 = CANVAS_WIDTH
@@ -47,11 +61,15 @@ class GUI:
 			x2 = k
 			self.canvas.create_line(x1, y1, x2, y2, fill="#000000")
 
-		self.textWidget.grid(row=1,column=0)
+		self.textWidget.grid(row=1,column=0,columnspan=2,)
 		self.canvas.grid(row=0,column=0,sticky="nw")
+		self.counters.grid(row=0,column=1,sticky="n")
 
 		receive_thread = Thread(target=self.receive)
 		receive_thread.start()
+
+		#counter_thread = Thread(target=self.printCounters)
+		#counter_thread.start()
 
 	"""
 	Receive a JSON string 
@@ -71,14 +89,25 @@ class GUI:
 		while True:
 			try:
 				msg = c.recv(BUFSIZ).decode()
-				#print(msg)
-				#print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+				self.receivedPacketCount += 1
+
 				# decode the JSON string
 				data = json.loads(msg)
 
+				self.intactPacketCount += 1
+
 				#self.newPacket(0, data['x'], data['y'], data['heading'], data['sig'], data['recent'], data['receiver'], data['elapsed'])
-				update = Thread(target=self.newPacket, args=(0, data['x'], data['y'], data['heading'], data['sig'], data['recent'], data['receiver'], data['elapsed'],))
+
+				if data['sig']:
+					self.authenticatedPacketCount += 1
+				if data['recent']:
+					self.ontimePacketCount += 1
+
+				update = Thread(target=self.newPacket, args=(0, data['x'], data['y'], data['heading'], data['sig'], data['recent'], data['receiver'], data['elapsed'],self.threadlock,))
 				update.start()
+
+				
 
 			except json.decoder.JSONDecodeError as jsonError:
 				print("JSON decoding error - invalid data. Discarding.")
@@ -93,7 +122,7 @@ class GUI:
 				print("=====================================================================================")
 
 
-	def newPacket(self, carid, x, y, heading, isValid, isRecent, isReceiver, elapsedTime):
+	def newPacket(self, carid, x, y, heading, isValid, isRecent, isReceiver, elapsedTime, lock):
 		
 		# cast coordinates to integers
 		x = int(x)
@@ -111,33 +140,43 @@ class GUI:
 
 		self.canvas.create_image(x, y, image=i, anchor=tk.CENTER, tags="car" + str(threading.currentThread().ident))
 		
-		# print results
-		if not isReceiver:
-			check = u'\u2713'
-			rejected = u'\u2716'
-			
-			self.textWidget.insert(tk.END, "==========================================\n","black")
-			if isValid:
+		with lock:
+			# print results
+			if not isReceiver:
+				check = u'\u2713'
+				rejected = u'\u2716'
 				
-				self.textWidget.insert(tk.END, check + "Message successfully authenticated\n","valid")
-			else:
-				self.textWidget.insert(tk.END, rejected + "Invalid signature!\n","attack")
-			
-			if isRecent:
-				self.textWidget.insert(tk.END, check + "Message is recent: " + str(round(elapsedTime,2)) + " micoseconds elapsed since transmission\n","valid")
-			else:	
-				self.textWidget.insert(tk.END, rejected + "Message out-of-date: " + str(round(elapsedTime,2)) + " micoseconds elapsed since transmission\n","information")
-			
-			if not isValid and not isRecent:
-				self.textWidget.insert(tk.END, rejected + "!!!--- Invalid signature AND message expired: replay attack likely! ---!!!\n","attack")
-			
-			self.textWidget.insert(tk.END, "Vehicle reports location at (" + str(x) + "," + str(y) + "), traveling " + heading + "\n", "black")
+				self.textWidget.insert(tk.END, "==========================================\n","black")
+				if isValid:
+					
+					self.textWidget.insert(tk.END, check + "Message successfully authenticated\n","valid")
+				else:
+					self.textWidget.insert(tk.END, rejected + "Invalid signature!\n","attack")
+				
+				if isRecent:
+					self.textWidget.insert(tk.END, check + "Message is recent: " + str(round(elapsedTime,2)) + " micoseconds elapsed since transmission\n","valid")
+				else:	
+					self.textWidget.insert(tk.END, rejected + "Message out-of-date: " + str(round(elapsedTime,2)) + " micoseconds elapsed since transmission\n","information")
+				
+				if not isValid and not isRecent:
+					self.textWidget.insert(tk.END, rejected + "!!!--- Invalid signature AND message expired: replay attack likely! ---!!!\n","attack")
+				
+				self.textWidget.insert(tk.END, "Vehicle reports location at (" + str(x) + "," + str(y) + "), traveling " + heading + "\n", "black")
 
-			self.textWidget.insert(tk.END, "==========================================\n","black")
-			self.textWidget.see(tk.END)
+				self.textWidget.insert(tk.END, "==========================================\n","black")
+				self.textWidget.see(tk.END)
 		time.sleep(1)
 		self.canvas.delete("car" + str(threading.currentThread().ident))
+		self.processedPacketCount += 1
 
+	def printCounters(self):
+		while True:
+			print(str(self.receivedPacketCount))
+			print(str(self.processedPacketCount))
+			print(str(self.authenticatedPacketCount))
+			print(str(self.intactPacketCount))
+			print(str(self.ontimePacketCount))
+			time.sleep(2)
 
 if __name__=="__main__":
 	s = socket()
