82c9adb676feb780dc48cc9a3e8a030e3c1ca0d4
Geoff Twardokus <gdt5762@rit.edu>
2020-05-31 15:44:30 -0400

    Fixed time display issue?

--- Verifier.py
+++ Verifier.py
@@ -13,18 +13,19 @@ class Verifier:
     
     # Returns true if less than 100ms elapsed between message transmission and reception
     def verifyTime(self, timestamp):
-        return (timestamp, self.calculateElapsedTime(timestamp) < 100)
+        elapsed = self.calculateElapsedTime(timestamp)
+        return (elapsed, elapsed < 100)
     
     # calculate the number of elapsed milliseconds since the message was transmitted
     def calculateElapsedTime(self, timeInMilliseconds):
-        unpaddedTimeInMicroseconds = ""
+        unpaddedTimeInMilliseconds = ""
         for i in range(0,len(timeInMilliseconds)):
             if timeInMilliseconds[i] != "0":
-                unpaddedTimeInMicroseconds = timeInMilliseconds[i:]
+                unpaddedTimeInMilliseconds = timeInMilliseconds[i:]
                 break
-        unpaddedTimeInMicroseconds = int(unpaddedTimeInMicroseconds, 16)
+        unpaddedTimeInMilliseconds = int(unpaddedTimeInMilliseconds, 16)
     
         origin = datetime(2004, 1, 1, 0, 0, 0, 0)
         now = (datetime.now() - origin).total_seconds() * 1000
     
-        return now - unpaddedTimeInMicroseconds
+        return now - unpaddedTimeInMilliseconds
