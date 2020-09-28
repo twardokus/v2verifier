607ca1df11317d79c2318c2ad947c74dfc91b7ee
Geoff Twardokus <gdt5762@rit.edu>
2020-09-28 13:58:28 -0400

    Major changes to remove GUI and non-signature validation checks from main execution on the receiver. Now the only output is a T/F print to the console for the signature validation of each received message.

--- Receiver.py
+++ Receiver.py
@@ -11,32 +11,34 @@ class Receiver:
         self.verifier = Verifier()
         self.messageCounter = 1
         
-    def runReceiver(self, socketToGUI, guiLock):
-        listener = Thread(target=self.listenForWSMs(socketToGUI, guiLock))
+    def runReceiver(self):
+        listener = Thread(target=self.listenForWSMs())
         listener.start()
         
-    def listenForWSMs(self, s, lock):
+    def listenForWSMs(self):
         # print a console message to confirm the network connection is active
         print("Listening on localhost:4444 for WSMs")
     
         # listen on localhost:4444 which is the UDP sink from GNURadio in wifi_rx.py
         # received packets are passed to processPacket() for data extraction
-        sniff(iface="lo", filter="dst port 4444", prn=lambda x: self.filterDuplicatePackets(str(binascii.hexlify(x.load))[130:],s,lock))
+        sniff(iface="lo", filter="dst port 4444", prn=lambda x: self.filterDuplicatePackets(str(binascii.hexlify(x.load))[130:]))
     
-    def filterDuplicatePackets(self, payload, s, lock):
+    def filterDuplicatePackets(self, payload):
         if self.messageCounter % 2 == 1:
-            self.processPacket(payload, s, lock)
+            self.processPacket(payload)
         self.messageCounter += 1
 
     # uses returned tuple from parseWSM to verify message and send to GUI
-    def processPacket(self, payload, s, lock):
+    def processPacket(self, payload):
     
         # extract the elements "(unsecuredData,r,s,time)" from the 1609.2 structure
         data = self.parseWSM(payload)
         
+        """
         #BSMData = data[0].decode('hex').replace("\n","").split(",")
         BSMData = bytes.fromhex(data[0]).decode('ascii').replace("\n","").split(",")
 
+        
         # create a dictionary to pack and send
         decodedData = {}    
         
@@ -45,24 +47,30 @@ class Receiver:
         decodedData['y'] = BSMData[2]
         decodedData['heading'] = BSMData[3]
         decodedData['speed'] = BSMData[4]
+        """
+    
+#       publicKey = keys.import_key("keys/" + decodedData['id'] + "/p256.pub",curve=curve.P256, public=True)
+        publicKey = keys.import_key("keys/0/p256.pub",curve=curve.P256, public=True)
 
-        publicKey = keys.import_key("keys/" + decodedData['id'] + "/p256.pub",curve=curve.P256, public=True)
-        
+
         # verify the signature
         isValidSig = self.verifier.verifySignature(data[1],data[2],data[0],publicKey)
         
-        elapsed, isRecent = self.verifier.verifyTime(data[3])
-        
+        #elapsed, isRecent = self.verifier.verifyTime(data[3])
+        """
         decodedData['sig'] = isValidSig
         decodedData['elapsed'] = elapsed
         decodedData['recent'] = isRecent
         decodedData['receiver'] = False
-    
+                
         vehicleDataJSON = json.dumps(decodedData)
     
         with lock:
             s.send(vehicleDataJSON.encode())
-  
+        """
+        
+        print(isValidSig)
+        
     # takes the hex payload from an 802.11p frame as an argument, returns tuple of extracted bytestrings
     def parseWSM(self, WSM):
             
