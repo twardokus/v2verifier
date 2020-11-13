df171040f7f008537f23e581dad1edc79f58c618
Geoff Twardokus <gdt5762@rit.edu>
2020-11-13 12:52:51 -0500

    Restored basic GUI functionality

--- Receiver.py
+++ Receiver.py
@@ -11,33 +11,33 @@ class Receiver:
         self.verifier = Verifier()
         self.messageCounter = 1
         
-    def runReceiver(self):
-        listener = Thread(target=self.listenForWSMs())
+    def runReceiver(self, s, lock):
+        listener = Thread(target=self.listenForWSMs(s, lock))
         listener.start()
         
-    def listenForWSMs(self):
+    def listenForWSMs(self, s, lock):
         # print a console message to confirm the network connection is active
         print("Listening on localhost:4444 for WSMs")
     
         # listen on localhost:4444 which is the UDP sink from GNURadio in wifi_rx.py
         # received packets are passed to processPacket() for data extraction
-        sniff(iface="lo", filter="dst port 4444", prn=lambda x: self.filterDuplicatePackets(str(binascii.hexlify(x.load))[130:]))
+        sniff(iface="lo", filter="dst port 4444", prn=lambda x: self.filterDuplicatePackets(str(binascii.hexlify(x.load))[130:], s, lock))
     
-    def filterDuplicatePackets(self, payload):
+    def filterDuplicatePackets(self, payload, s, lock):
         if self.messageCounter % 2 == 1:
-            self.processPacket(payload)
+            self.processPacket(payload, s, lock)
         self.messageCounter += 1
 
     # uses returned tuple from parseWSM to verify message and send to GUI
-    def processPacket(self, payload):
+    def processPacket(self, payload, s, lock):
     
         # extract the elements "(unsecuredData,r,s,time)" from the 1609.2 structure
         data = self.parseWSM(payload)
-        
+        """
         if data == -1:
             return
-        
         """
+        
         #BSMData = data[0].decode('hex').replace("\n","").split(",")
         BSMData = bytes.fromhex(data[0]).decode('ascii').replace("\n","").split(",")
 
@@ -50,7 +50,7 @@ class Receiver:
         decodedData['y'] = BSMData[2]
         decodedData['heading'] = BSMData[3]
         decodedData['speed'] = BSMData[4]
-        """
+        
     
 #       publicKey = keys.import_key("keys/" + decodedData['id'] + "/p256.pub",curve=curve.P256, public=True)
         publicKey = keys.import_key("keys/0/p256.pub",curve=curve.P256, public=True)
@@ -59,8 +59,8 @@ class Receiver:
         # verify the signature
         isValidSig = self.verifier.verifySignature(data[1],data[2],data[0],publicKey)
         
-        #elapsed, isRecent = self.verifier.verifyTime(data[3])
-        """
+        elapsed, isRecent = self.verifier.verifyTime(data[3])
+        
         decodedData['sig'] = isValidSig
         decodedData['elapsed'] = elapsed
         decodedData['recent'] = isRecent
@@ -70,9 +70,6 @@ class Receiver:
     
         with lock:
             s.send(vehicleDataJSON.encode())
-        """
-        
-        print(isValidSig)
         
     # takes the hex payload from an 802.11p frame as an argument, returns tuple of extracted bytestrings
     def parseWSM(self, WSM):
--- local_main.py
+++ local_main.py
@@ -1,18 +1,13 @@
 # this is the main execution file for the "local vehicle"
 
 import os
-"""
-import sys
-
-
-print(os.getcwd())
-sys.path.append("/home/administrator/eclipse-workspace/v2verifier/v2verifier")
-"""
-
 import yaml
 from threading import Lock, Thread
 from socket import socket
 from Receiver import Receiver
+from LocalVehicle import LocalVehicle
+from GUI import GUI
+import tkinter as tk
 
 
 #from LocalVehicle import LocalVehicle
@@ -26,31 +21,27 @@ if __name__=="__main__":
         print("Error - you must be root! Try running with sudo")
         exit(1)
     
-    """
+    
     root = tk.Tk()
     gui = GUI(root)
     gui.runGUIReceiver()
     print("GUI Initialized...")
     
-    
     s2 = socket()
     s2.connect(('127.0.0.1',6666))
-    """
     
     lock = Lock()
     
     receiver = Receiver()
     
-    listener = Thread(target=receiver.runReceiver())
+    listener = Thread(target=receiver.runReceiver, args=(s2, lock,))
     listener.start()
     print("Listener running...")
     
-    """
     lv = LocalVehicle(config["localConfig"]["tracefile"])
     
     local = Thread(target=lv.start, args=(s2,lock,))
     local.start()
     
-    
     root.mainloop()
-    """
+    
