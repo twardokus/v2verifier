a37a35eeea67e8dc73e39e006356624b2b4b7aa2
Jack Sauriol <jack@sauriol.net>
2021-04-30 03:49:04 -0700

    Merge gui changes from branch gui-dev into capstone development branch misc_dev (#18)
    Replaces tkinter gui with web-based gui + moves some vehicle code into main.py
    
    * untested gui code
    
    * remove api key
    
    * add <p> tags to messages
    
    * fix imports
    
    * fix formatting + spelling
    
    * smh my head indentation
    
    * Added some more debugging output
    
    * Replace gui stuff
    
    * Make gui nicer + fix test
    
    * Percentage calculations

---
+++ gui.py
@@ -0,0 +1,191 @@
+import eel
+import threading
+import yaml
+import folium
+import time
+import socket
+import logging
+import json
+
+
+class GUI:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        self.logger.addHandler(ch)
+
+        self.threadlock = threading.Lock()
+
+        with open("init.yml", "r") as conf_file:
+            self.config = yaml.load(conf_file, Loader=yaml.FullLoader)
+
+        self.num_vehicles = self.config["remoteConfig"]["numberOfVehicles"] + 1
+        self.totalPackets = self.config["remoteConfig"]["traceLength"] * (
+            self.num_vehicles - 1
+        )
+
+        self.received_packets = 0
+        self.processed_packets = 0
+        self.authenticated_packets = 0
+        self.intact_packets = 0
+        self.ontime_packets = 0
+
+        self.logger.info("Initialized GUI")
+
+    def update_vehicle(self, vehicle_id, lat, lng, icon_path):
+        self.logger.info(f"moving vehicle {vehicle_id} to {lat}, {lng}")
+        eel.updateMarker(vehicle_id, lat, lng, icon_path)
+
+    def update_packet_counts(self, received, processed, authenticated, intact, ontime):
+        self.logger.info("called update_packet_counts in python")
+        self.received_packets = received
+        self.processed_packets = processed
+        self.authenticated_packets = authenticated
+        self.intact_packets = intact
+        self.ontime_packets = ontime
+
+    def add_message(self, message):
+        eel.addMessage(message)
+
+    def prep(self):
+        eel.init("gui")
+
+    def run(self):
+        self.logger.info("called run, starting server")
+        self.logger.info("starting for real")
+        eel.start(
+            "main.html",
+            mode="custom",
+            cmdline_args=["firefox", "http://localhost:8000"],
+        )
+
+    def start_receiver(self):
+        self.logger.info("called start_receiver, creating socket")
+        self.sock = socket.socket()
+        self.sock.bind(("127.0.0.1", 6666))
+
+        # eel.spawn(self.update_stats_labels)
+        label_thread = threading.Thread(target=self.update_stats_labels)
+        label_thread.start()
+
+        # eel.spawn(self.receive)
+        self.receiver = threading.Thread(target=self.receive)
+        self.receiver.start()
+
+    def update_stats_labels(self):
+        self.logger.info("starting update_stats_labels")
+
+        while True:
+
+            if self.received_packets == 0:
+                continue
+
+            eel.updatePacketCounts(
+                self.received_packets,
+                self.processed_packets,
+                self.authenticated_packets,
+                self.intact_packets,
+                self.ontime_packets,
+            )
+
+            eel.sleep(0.1)
+
+    def receive(self):
+        self.logger.info("starting receive")
+
+        BUFFER_SIZE = 200
+
+        self.sock.listen(4)
+        conn = self.sock.accept()[0]
+
+        while True:
+            try:
+                msg = conn.recv(BUFFER_SIZE).decode()
+                data = json.loads(msg)
+
+                self.logger.info("received data")
+
+                if not data["receiver"]:
+                    self.received_packets += 1
+                    self.intact_packets += 1
+
+                    if data["sig"]:
+                        self.authenticated_packets += 1
+
+                    if data["recent"]:
+                        self.ontime_packets += 1
+
+                update = threading.Thread(
+                    target=self.process_new_packet,
+                    args=(
+                        data["id"],
+                        data["x"],
+                        data["y"],
+                        data["heading"],
+                        data["sig"],
+                        data["recent"],
+                        data["receiver"],
+                        data["elapsed"],
+                    ),
+                )
+                update.start()
+
+            except json.decoder.JSONDecodeError:
+                self.logger.error("JSON decoding error, discarding invalid data")
+            except Exception as e:
+                self.logger.error(f"exception: {e}")
+
+    def process_new_packet(
+        self,
+        vehicle_id,
+        lat,
+        lng,
+        heading,
+        is_valid,
+        is_recent,
+        is_receiver,
+        elapsed_time,
+    ):
+        self.logger.info(f"processing packet from {vehicle_id}")
+
+        icon = ""
+        if is_receiver:
+            icon = f"/images/receiver/{heading}.png"
+        elif is_valid:
+            icon = f"/images/regular/{heading}.png"
+        else:
+            icon = f"/images/phantom/{heading}.png"
+
+        self.update_vehicle(vehicle_id, lat, lng, icon)
+
+        # print messages to gui
+
+        # acquire lock
+
+        message = f"<p>Message from {vehicle_id}:</p>"
+        if not is_receiver:
+            if is_valid:
+                message += '<p class="tab">✔️ Message successfully authenticated</p>'
+            else:
+                message += '<p class="tab">❌ Invalid signature!\n</p>'
+
+            if is_recent:
+                rounded_time = 0
+                if elapsed_time > 0:
+                    rounded_time = str(round(elapsed_time, 2))
+
+                message += f'<p class="tab">✔️ Message is recent: {rounded_time} ms since transmission<p>'
+
+            else:
+                rounded_time = str(round(elapsed_time, 2))
+                message += '<p class="tab">❌ Message is out-of-date: {rounded_time} ms since transmission<p>'
+
+            if not is_valid and not is_recent:
+                message += '<p class="tab">❌❌❌ Invalid signature and message expired, replay attack likely ❌❌❌</p>'
+
+            message += f'<p class="tab">Vehicle reports location at {lat}, {lng} traveling {heading}<p>'
+            self.add_message(message)
+        else:
+            self.processed_packets += 1
--- main.py
+++ main.py
@@ -1,28 +1,125 @@
+#!/usr/bin/env python3
+
 import argparse
 import os
+import yaml
+import random
+import threading
+import time
+import socket
+from gui import GUI
+from Utility import Utility
+from Receiver import Receiver
+from RemoteVehicle import RemoteVehicle
+from LocalVehicle import LocalVehicle
+from multiprocessing import Process
 
-from Local import Local
-from Remote import Remote
 
-if __name__ == "__main__":
-
-    if os.geteuid() != 0:
-        raise Exception("Error - you must be root! Try running with sudo")
-
-    description = "Run a V2V security experiment using V2Verifier."
-
-    parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("perspective",
-                        help="choice of perspective",
-                        choices=["local", "remote"]
-                        )
-
+def main():
+    parser = argparse.ArgumentParser(
+        description="Run a V2V security experiment using V2Verifier"
+    )
+    parser.add_argument(
+        "perspective", help="choice of perspective", choices=["local", "remote", "test"]
+    )
     args = parser.parse_args()
 
-    if args.perspective == "local":
-        program = Local()
-        program.run_local()
+    if os.geteuid() != 0:
+        raise Exception("Error: rerun as root or with sudo")
 
+    try:
+        conf_file = open("init.yml", "r")
+        config = yaml.load(conf_file, Loader=yaml.FullLoader)
+    except Exception as e:
+        print(f"Unable to load config file init.yml: {e}")
+
+    if args.perspective == "local":
+        gui = GUI()
+        gui.start_receiver()
+        gui.prep()
+        time.sleep(1)
+
+        sock = socket.socket()
+        sock.connect(("127.0.0.1", 6666))
+
+        lock = threading.Lock()
+        receiver = Receiver()
+
+        listener = threading.Thread(target=receiver.run_receiver, args=(sock, lock))
+        listener.start()
+        print("Listener running")
+
+        lv = LocalVehicle(config["localConfig"]["tracefile"])
+        local = threading.Thread(target=lv.start, args=(sock, lock))
+        local.start()
+
+        gui.run()
     elif args.perspective == "remote":
-        program = Remote()
-        program.run_remote()
+        util = Utility()
+
+        remote_vehicles = []
+        vehicle_processes = []
+
+        try:
+            for i in range(0, config["remoteConfig"]["numberOfVehicles"]):
+                trace_file_path = config["remoteConfig"]["traceFiles"][i]
+                bsm_queue = util.build_bsm_queue(
+                    i, trace_file_path, f"keys/{i}/p256.key"
+                )
+                rv = RemoteVehicle(bsm_queue)
+                remote_vehicles.append(rv)
+        except IndexError:
+            print(
+                f"Error starting vehicles. The config file is missing a trace file or BSM file path for vehicle {i} in init.yml"
+            )
+
+            for rv in remote_vehicles:
+                vehicle = Process(target=rv.start)
+            vehicle_processes.append(vehicle)
+            vehicle.start()
+            print("Started legitimate vehicle")
+
+        for vehicle in vehicle_processes:
+            vehicle.join()
+
+        print("All vehicle processes terminated")
+    elif args.perspective == "test":
+        gui = GUI()
+        gui.start_receiver()
+        gui.prep()
+        time.sleep(1)
+        print("Created gui object, calling run")
+        gui_thread = threading.Thread(target=gui.run)
+        gui_thread.start()
+        print("thread started")
+
+        with open("coords/coords_1") as coord_file:
+            while True:
+                for line in coord_file:
+                    lat, lng = line.strip().split(", ")
+                    # update car location
+                    gui.process_new_packet("car_1", lat, lng, "N", True, True, True, 0)
+
+                    # add message
+                    message = "<p>Message from car_1:</p>"
+                    message += (
+                        '<p class="tab">✔️ Message successfully authenticated</p>'
+                    )
+                    message += '<p class="tab">✔️ Message is recent: 31.6 ms since transmission</p>'
+                    message += f'<p class="tab">Vehicle reports location at {lat}, {lng} traveling N</p>'
+                    gui.add_message(message)
+
+                    # update packet counts
+                    gui.received_packets += 1
+                    gui.intact_packets += 1
+                    if random.random() <= 0.8:
+                        gui.authenticated_packets += 1
+                    gui.ontime_packets += 1
+
+                    time.sleep(0.5)
+
+        gui_thread.join()
+
+
+if __name__ == "__main__":
+    main()
