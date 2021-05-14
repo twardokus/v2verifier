6234d661b1e0a29226c7484e9a62c9d7774d33f3
Geoff Twardokus <gdt5762@rit.edu>
2021-05-14 12:40:53 -0400

    Complete parser function and add docstring

--- v2verifier/V2VReceive.py
+++ v2verifier/V2VReceive.py
@@ -1,7 +1,19 @@
 import V2VTransmit
+import struct
+
 
 def parse_received_spdu(spdu: bytes) -> tuple:
-    llc_format_string = "HbxxxH"
+    """Parse a 1609.2 SPDU
+
+    Parameters:
+        spdu (bytes): a 1609.2 SPDU
+
+    Returns:
+        tuple: the contents of the SPDU
+
+    """
+
+    llc_format_string = ">HbxxxH"
     wsm_header_format_string = "bbbb"
 
     ieee1609_dot2_data_format_string = "bBbbbBB"
@@ -14,7 +26,12 @@ def parse_received_spdu(spdu: bytes) -> tuple:
 
     payload, signature = spdu[:-64], spdu[-64:]
 
+    # spdu_contents will be a tuple containing each element of the 1609.2 SPDU as constructed
+    # using V2VTransmit.generate_1609_spdu()
+    spdu_contents = struct.unpack(spdu_format_string, payload)
+
+    return spdu_contents
 
 
 if __name__=="__main__":
-    parse_received_spdu(V2VTransmit.generate_1609_spdu(b'test',0))
+    parse_received_spdu(V2VTransmit.generate_1609_spdu(V2VTransmit.generate_v2v_bsm(43, -71, 1543, 45.36, 145.223), 21))
