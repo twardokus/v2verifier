713f169b5f30c5255e91f7dd5ead21adc87b6514
geoff <gdt5762@rit.edu>
2022-02-17 11:35:43 -0500

    Add CPP files.

---
+++ v2verifier_cpp/bsm.h
@@ -0,0 +1,16 @@
+//
+// Created by geoff on 10/19/21.
+//
+
+#ifndef CPP_BSM_H
+#define CPP_BSM_H
+
+struct bsm {
+    float latitude;
+    float longitude;
+    float elevation;
+    float speed;
+    float heading;
+};
+
+#endif //CPP_BSM_H
