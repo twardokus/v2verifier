6389842d1e53014e0c46d1e1291d7bc7fa1444fd
Geoff Twardokus <gdt5762@rit.edu>
2022-05-06 10:33:45 -0400

    Delete old files for command-line arguments.

--- v2verifier_cpp/main.cpp
+++ v2verifier_cpp/main.cpp
@@ -2,28 +2,62 @@
 #include <thread>
 
 #include "Vehicle.h"
-#include "VehicleUtil.h"
+#include "arguments.h"
+
+//#include "VehicleUtil.h"
+
+void print_usage() {
+    std::cout << "Usage: v2verifer {dsrc | cv2x} {transmitter | receiver} [--test] " << std::endl;
+}
 
 int main(int argc, char *argv[]) {
 
-    int num_msgs = 10000;
-
-    ArgumentParser Parsed_args;
-    if(argc == 7 || argc == 6 || argc == 2)
-        Parsed_args = parse_args(argc,argv);
-    else{
-        perror("1(-h), 5 or 6 arguments are required for execution\n");
+    if(argc < 3 || argc > 4) {
+        print_usage();
         exit(EXIT_FAILURE);
     }
 
-    if(Parsed_args.perspective==TRANSMITTER) {
-        Vehicle v1;
-        v1.transmit(num_msgs, Parsed_args);
-        v1.get_average_sign_times();
+    program_arguments args;
+
+    if(std::string(argv[1]) == "dsrc")
+        args.tech_choice = DSRC;
+    else if(std::string(argv[1]) == "cv2x")
+        args.tech_choice = CV2X;
+    else {
+        std::cout << "Error: first argument must be DSRC or C_V2X" << std::endl;
+        print_usage();
+        exit(EXIT_FAILURE);
     }
-    else if (Parsed_args.perspective==RECEIVER) {
-        Vehicle v1;
-        v1.receive(num_msgs, Parsed_args);
-        v1.get_average_verify_times();
+
+    if(std::string(argv[2]) == "transmitter")
+        args.sim_mode = TRANSMITTER;
+    else if(std::string(argv[2]) == "receiver")
+        args.sim_mode = RECEIVER;
+    else {
+        std::cout << R"(Error: second argument must be "transmitter" or "receiver")" << std::endl;
+        print_usage();
+        exit(EXIT_FAILURE);
     }
+
+    if(argc == 4) {
+        if(std::string(argv[3]) == "--test")
+            args.test = true;
+        else {
+            std::cout << R"(Error: optional third argument can only be "--test")" << std::endl;
+            print_usage();
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    int num_msgs = 10000;
+
+    if(args.sim_mode == TRANSMITTER) {
+        Vehicle v1;
+        v1.transmit(num_msgs, args.test);
+    }
+    else if (args.sim_mode == RECEIVER) {
+        Vehicle v1;
+        v1.receive(num_msgs, args.test);
+    }
+
 }
