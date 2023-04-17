b972cfc6ebe4d225a11e0c2953f6ccc6dd8cddbe
btg5907 <btg5907@rit.edu>
2023-04-17 15:31:02 -0400

    Vehicle::transmitLearnRequest() now splices common_cert_fields to ensure no byte-level inconsistencies between runs.

--- src/Vehicle.cpp
+++ src/Vehicle.cpp
@@ -79,19 +79,24 @@ void Vehicle::transmitLearnRequest(bool test) {
     ecdsa_spdu spdu;
     generate_ecdsa_spdu(spdu, 0);
 
-    //TODO: Once certs are fully utilized, this should a randomly chosen hash of one of the certs (only last 3 bytes)
-    printHex(&spdu.data.signedData.cert.commonCertFields, sizeof(spdu.data.signedData.cert.commonCertFields));
+    //TODO: this splicing should probably be moved to when the certificate is created.
+    ((uint16_t *)&spdu.data.signedData.cert.commonCertFields)[1] = 0;
+    ((uint16_t *)&spdu.data.signedData.cert.commonCertFields)[11] = 0;
+    ((uint16_t *)&spdu.data.signedData.cert.commonCertFields)[5] = 0x84;
+    printHex(&spdu.data.signedData.cert, sizeof(spdu.data.signedData.cert));
+
     unsigned char hash[SHA256_DIGEST_LENGTH];
     sha256sum(&spdu.data.signedData.cert, sizeof(spdu.data.signedData.cert), hash);
+
     unsigned char certHash[4];
     certHash[0] = hash[29];
     certHash[1] = hash[30];
     certHash[2] = hash[31];
-    //std::cout << sizeof(hash) << std::endl;
-    for (int i = 0; i < sizeof(hash); i++) std::cout << hash[i]; std::cout << std::endl;
-    std::cout << std::hex << certHash << std::endl << std::flush;
+
+    printHex(hash, sizeof(hash));
+    printHex(certHash, sizeof(certHash));
+
     strncpy(spdu.data.signedData.tbsData.headerInfo.p2pLearningRequest, (const char *) certHash, 4);
-    //std::cout << "Time since epoch in cert: " << std::chrono::system_clock::to_time_t(spdu.data.signedData.cert.commonCertFields.validity_period_start) << std::endl;
 
     sendto(sockfd, (struct ecdsa_spdu *) &spdu, sizeof(spdu), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
 
@@ -499,6 +504,7 @@ void Vehicle::load_trace(int number) {
 /**
  * Debugging method for checking byte-level values. Added to investigate inconsistent hash values.
  * Prints hexadecimal values in batches of shorts with alignment.
+ * ***PLEASE NOTE bytes in each short are reversed from how you expect them to print. 'hostname' gets printed as 'ohtsanem' in hex values.***
  * @param ptr A pointer to the object to print out
  * @param size The size of the object in bytes. sizeof() will suffice.
  */
@@ -506,7 +512,7 @@ void Vehicle::printHex(void* ptr, int size) {
     int N = size % 2 == 1 ? size / 2 + 1 : size / 2; // never tested with an odd size just beware
 
     for (int i = 0; i < N; i++) {
-        if (i % 4 == 0) std::cout << std::endl;
+        if (i % 4 == 0) std::cout << std::endl; //new line every 4 shorts/8bytes to show memory alignment
         uint16_t c = ((uint16_t *)ptr)[i]; // We cast ptr to uint16_t* to treat it as an array of shorts
         std::cout << std::hex << c << ' ';
     }
