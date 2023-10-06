7b9825da263711a2c8e216169da93991f954a43e
Geoff Twardokus <gdt5762@rit.edu>
2023-10-06 12:37:54 -0400

    More restructuring + integrating EEL into LibreGL.

---
+++ open-street-map-gui/gui.py
@@ -0,0 +1,11 @@
+import eel
+
+
+eel.init(path='webapp',
+         allowed_extensions=['.js', 'html', '.css'])
+
+
+
+# Start the webapp (will open in Chrome/Chromium)
+eel.start('osm-gui.html')
+
---
+++ open-street-map-gui/webapp/load-map.js
@@ -0,0 +1,100 @@
+var longitudeRIT = -77.675250;
+var latitudeRIT = 43.084132;
+
+const bounds = [
+    [-77.688972, 43.072019], // Southwest coordinates
+    [-77.663638, 43.092922] // Northeast coordinates
+];
+
+const map = (window.map = new maplibregl.Map({
+    container: 'map',
+    zoom: 14.85,
+    center: [longitudeRIT, latitudeRIT],
+    pitch: 0,
+    hash: true,
+    style: {
+        version: 8,
+        sources: {
+            osm: {
+                type: 'raster',
+                tiles: ['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
+                tileSize: 256,
+                attribution: '&copy; OpenStreetMap Contributors',
+                maxzoom: 50
+            },
+            // Use a different source for terrain and hillshade layers, to improve render quality
+            terrainSource: {
+                type: 'raster-dem',
+                url: 'https://demotiles.maplibre.org/terrain-tiles/tiles.json',
+                tileSize: 256
+            },
+            hillshadeSource: {
+                type: 'raster-dem',
+                url: 'https://demotiles.maplibre.org/terrain-tiles/tiles.json',
+                tileSize: 256
+            }
+        },
+        layers: [
+            {
+                id: 'osm',
+                type: 'raster',
+                source: 'osm'
+            },
+            {
+                id: 'hills',
+                type: 'hillshade',
+                source: 'hillshadeSource',
+                layout: {visibility: 'visible'},
+                paint: {'hillshade-shadow-color': '#473B24'}
+            }
+        ],
+        terrain: {
+            source: 'terrainSource',
+            exaggeration: 1
+        }
+    },
+    maxZoom: 18,
+    maxPitch: 85,
+    maxBounds: bounds
+}));
+
+const geojson = {
+    'type': 'FeatureCollection',
+    'features': [
+        {
+            'type': 'Feature',
+            'properties': {
+                'message': 'Foo',
+                'iconSize': [60, 60]
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-77.681562, 43.081050]
+            }
+        }
+    ]
+};
+
+
+// add markers to map
+geojson.features.forEach((marker) => {
+    // create a DOM element for the marker
+    const el = document.createElement('div');
+    el.className = 'marker';
+    el.style.backgroundImage =
+        `url(/v2verifier/web/images/regular/E.png)`;
+    el.style.width = `${marker.properties.iconSize[0]}px`;
+    el.style.height = `${marker.properties.iconSize[1]}px`;
+    el.style.backgroundRepeat = `no-repeat`;
+
+    el.addEventListener('click', () => {
+        window.alert(marker.properties.message);
+    });
+
+    // add marker to map
+    new maplibregl.Marker({element: el})
+        .setLngLat(marker.geometry.coordinates)
+        .addTo(map);
+});
+
+map.scrollZoom.disable();
