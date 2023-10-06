97e40082145a7ab7adc1754695f664f3c9a5feed
Geoff Twardokus <gdt5762@rit.edu>
2023-10-06 10:00:52 -0400

    Add initial OSM test files.

---
+++ osm/test.js
@@ -0,0 +1,31 @@
+// Define the map syle (OpenStreetMap raster tiles)
+const style = {
+  "version": 8,
+	"sources": {
+    "osm": {
+			"type": "raster",
+			"tiles": ["https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
+			"tileSize": 256,
+      "attribution": "&copy; OpenStreetMap Contributors",
+      "maxzoom": 19
+    }
+  },
+  "layers": [
+    {
+      "id": "osm",
+      "type": "raster",
+      "source": "osm" // This must match the source key above
+    }
+  ]
+};
+
+// Initialise the map
+const map = new maplibregl.Map({
+  container: 'map',
+  style: style,
+  center: [1, 15],
+  zoom: 3
+});
+
+// Add the navigation control
+map.addControl(new maplibregl.NavigationControl());
