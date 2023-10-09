a022db1b5e172c191f0ba0d800929a14b3dc3d70
Geoff Twardokus <geoff.twardokus@outlook.com>
2023-10-09 12:42:30 -0400

    Update and restructure the main GUI execution file.

--- open-street-map-gui/gui.py
+++ open-street-map-gui/gui.py
@@ -1,10 +1,42 @@
 import eel
+import pandas as pd
+import os
+
+from src.Vehicle import Vehicle
 
 
-eel.init(path='webapp',
-         allowed_extensions=['.js', 'html', '.css'])
+def render_all_vehicles(vehicles: list[Vehicle]) -> None:
+    eel.reset_markers()
+    for v in vehicles:
+        eel.add_new_vehicle(v.longitude, v.latitude, v.heading)
+    eel.update_markers()
 
 
+def main():
 
-# Start the webapp (will open in Chrome/Chromium)
-eel.start('osm-gui.html')
+    # Initialize EEL
+    eel.init(path='webapp',
+             allowed_extensions=['.js', 'html', '.css'])
+
+    # Start the webapp (will open in Chrome/Chromium)
+    eel.start('osm-gui.html', block=False)
+
+    vehicle_test_data = pd.read_excel(os.path.join(os.getcwd(),
+                                                   'data',
+                                                   'vehicle_test_data.xlsx')
+                                      )
+
+    vehicles = []
+
+    for i in range(len(vehicle_test_data)):
+        vehicles.append(Vehicle(longitude=vehicle_test_data.loc[i, 'longitude'],
+                                latitude=vehicle_test_data.loc[i, 'latitude'],
+                                heading=vehicle_test_data.loc[i, 'heading']))
+
+    while True:
+        eel.sleep(1)
+        render_all_vehicles(vehicles)
+
+
+if __name__ == "__main__":
+    main()
