7b3ed55e43a04d559e64547db3b37a9699e3cf31
Geoff Twardokus <geoff.twardokus@outlook.com>
2024-03-12 11:20:27 -0400

    A couple of new files

---
+++ v2xmessage/include/V2XMessageService.hpp
@@ -0,0 +1,19 @@
+//
+// Created by Geoff Twardokus on 3/11/24.
+//
+
+#ifndef V2VERIFIER_V2XMESSAGESERVICE_HPP
+#define V2VERIFIER_V2XMESSAGESERVICE_HPP
+
+
+class V2XMessageService {
+
+public:
+    V2XMessageService() = default;
+
+
+
+};
+
+
+#endif //V2VERIFIER_V2XMESSAGESERVICE_HPP
---
+++ v2xmessage/src/V2XMessageService.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Geoff Twardokus on 3/11/24.
+//
+
+#include "../include/V2XMessageService.hpp"
+
