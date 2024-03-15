8a6ca8e23d8f6d7d0f36827d3562aa23ebea7442
Geoff Twardokus <geoff.twardokus@outlook.com>
2024-03-14 20:47:01 -0400

    Documentation.

--- v2xmessage/include/EccP256CurvePoint.hpp
+++ v2xmessage/include/EccP256CurvePoint.hpp
@@ -1,39 +1,50 @@
-//
-// Created by Geoff Twardokus on 3/7/24.
-//
-/*
-EccP256CurvePoint::= CHOICE {
-    x-only           OCTET STRING (SIZE (32)),
-    fill             NULL,
-    compressed-y-0   OCTET STRING (SIZE (32)),
-    compressed-y-1   OCTET STRING (SIZE (32)),
-    uncompressedP256 SEQUENCE {
-      x OCTET STRING (SIZE (32)),
-      y OCTET STRING (SIZE (32))
-    }
-}
+/** @file EccP256CurvePoint.hpp
+ *  @brief Implementation of the ECCP256CurvePoint ASN.1 defined in IEEE 1609.2-2022.
+ *
+ *  @author Geoff Twardokus
+ *
+ *  @bug no known bugs
 */
+
+//EccP256CurvePoint::= CHOICE {
+//    x-only           OCTET STRING (SIZE (32)),
+//    fill             NULL,
+//    compressed-y-0   OCTET STRING (SIZE (32)),
+//    compressed-y-1   OCTET STRING (SIZE (32)),
+//    uncompressedP256 SEQUENCE {
+//        x OCTET STRING (SIZE (32)),
+//        y OCTET STRING (SIZE (32))
+//    }
+//}
 
 #ifndef V2VERIFIER_ECCP256CURVEPOINT_HPP
 #define V2VERIFIER_ECCP256CURVEPOINT_HPP
 
 #include "V2XMessage.hpp"
 
+/** @brief Options for how a curve point can be encoded under IEEE 1609.2 */
 enum CurvePointChoice {
-    xOnly,
-    fillNull,
-    compressedY0,
-    compressedY1,
-    uncompressedP256
+    xOnly,              ///< x-coordinate of the point as a 32-byte unsigned integer
+    fillNull,           ///< no value (NULL) is provided
+    compressedY0,       ///< y-coordinate of the point as a 32-byte unsigned integer when LSB is 0
+    compressedY1,       ///< y-coordinate of the point as a 32-byte unsigned integer when LSB is 1
+    uncompressedP256    ///< x- and y- coordinate provided in sequence as 32-byte unsigned integers
 };
 
 class EccP256CurvePoint {
 
 public:
 
+    /// Size of the COER representation for instantiations of this object (in bytes)
     static const uint16_t ECC_P256_CURVE_POINT_SIZE_BYTES = 33;
 
+    /** @brief Default constructor */
     EccP256CurvePoint() = default;
+
+    /** @brief Create a new ECCP256CurvePoint for a COER-encoded byte string
+     *
+     *  @param coerBytes COER encoding of an ECCP256CurvePoint object
+     */
     EccP256CurvePoint(std::vector<std::byte> &coerBytes) {
         if(coerBytes.size() != ECC_P256_CURVE_POINT_SIZE_BYTES) {
             throw std::runtime_error("Invalid COER (wrong length) provided for EccP256CurvePoint");
@@ -62,6 +73,10 @@ public:
         }
     }
 
+    /** @brief Get the object's COER representation as a byte string
+     *
+     *  @return COER encoding of the object.
+     */
     std::vector<std::byte> getCOER() {
         std::vector<std::byte> coerBytes;
 
@@ -73,17 +88,27 @@ public:
         return coerBytes;
     }
 
+    /** @brief Get the chosen curve point choice for this object
+     *
+     *  @return The CurvePointChoice for the object.
+     */
     CurvePointChoice getCurvePointChoice() const {
         return this->curvePointChoice;
     }
 
+    /** @brief  Get the compressed coordinate value for this curve point.
+     *
+     *  This could be x- or y-coordinate, depending on the selection for this->curvePointChoice. Queries should first
+     *  call EccP256CurvePoint::getCurvePointChoice() to anticipate the correct coordinate encoding.
+     *
+     *  @return The compressed x- or y-coordinate value for this curve point.
+     */
     std::vector<std::byte> getCompressedValue() const {
         return this->compressedValue;
     }
 
 private:
     CurvePointChoice curvePointChoice;
-
     std::vector<std::byte> compressedValue;
 };
 
--- v2xmessage/include/V2XMessage.hpp
+++ v2xmessage/include/V2XMessage.hpp
@@ -1,6 +1,13 @@
-//
-// Created by Geoff Twardokus on 3/6/24.
-//
+/** @file   V2XMessage.hpp
+ *  @brief  Abstract parent class for V2X message classes
+ *
+ *  All classes for SPDU elements defined in IEEE 1609.2, as well as specific message types, inherit from this abstract
+ *  class.
+ *
+ *  @author Geoff Twardokus
+ *
+ *  @bug    no known bugs
+ */
 
 #ifndef V2VERIFIER_V2XMESSAGE_HPP
 #define V2VERIFIER_V2XMESSAGE_HPP
@@ -16,9 +23,18 @@
 class V2XMessage {
 
 public:
+    /** @brief  Default constructor for V2XMessage.
+     *
+     *  @param  None.
+     */
     V2XMessage() = default;
 
 private:
+
+    /** @brief  Get the COER bytestring for the object.
+     *
+     *  @return The COER encoding of the object as a byte string.
+     */
     virtual std::vector<std::byte> getCOER() = 0;
 
 };
